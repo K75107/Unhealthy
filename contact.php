f55dbb63224e42bbcc21f37c9cf248118f106c9f
K75107 <divinagracia.kent123@gmail.com>
2023-06-21 07:40:20 +0800

    v5

---
+++ contact.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+  <!-- CSS -->
+    <link rel="stylesheet" href="css/global.css">
+
+    <link rel="stylesheet" media="(max-width: 720px)" href="css/contact-small.css">
+    <link rel="stylesheet" media="(max-width: 720px)" href="css/global-small.css">
+
+    <link rel="stylesheet" media="(min-width: 721px)" href="css/contact-wide.css">
+    <link rel="stylesheet" media="(min-width: 721px)" href="css/global-wide.css">
+
+
+  <!-- FONTS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css" integrity="sha512-Z0kTB03S7BU+JFU0nw9mjSBcRnZm2Bvm0tzOX9/OuOuz01XQfOpa0w/N9u6Jf2f1OAdegdIPWZ9nIZZ+keEvBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Montserrat&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+  
+</head>
+
+<body>
+
+  <?php
+        include 'header.php';
+  ?>
+
+    <div class="display">
+        <div class="slide_frame">
+            <div class="slideshow">
+                <div class="slide first">
+                    <div class="text_holder"></div>
+                </div>
+                <div class="slide second">
+
+                </div>
+                <div class="slide third">
+
+                </div>
+            </div>
+        </div>
+      
+        <div class="message_holder">
+
+        </div>
+
+  
+    </div>
+    <?php
+        include 'footer.php';
+  ?>
+    <script src="js/script.js"></script>
+
+
+</body>
+</html>
